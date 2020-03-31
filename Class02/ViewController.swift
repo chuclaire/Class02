96f7eff957e609a109fa8bf16312fe68ec574769
朱詩婷 <chi00claire@gmail.com>
2020-03-31 15:06:07 +0800

    Initial Commit

---
+++ Class02/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Class02
+//
+//  Created by ntust02 on 2020/3/31.
+//  Copyright © 2020 朱詩婷. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
